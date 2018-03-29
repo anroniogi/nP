521ef127dfb27818104003c142d29556b4ab3be1
mariaross <anroniogi@icoud.com>
2018-03-29 16:10:52 +0900

    2018-03-29-quit명령어

---
+++ tserver.c
@@ -0,0 +1,48 @@
+#include "headerFiles.h"
+
+#define BUFF_SIZE 256
+#define PORT_NUMBER 50000
+
+int   main( void)
+{
+   int   sock;
+   int   client_addr_size;
+
+   struct sockaddr_in   server_addr;
+   struct sockaddr_in   client_addr;
+
+   char   buff_rcv[BUFF_SIZE+1];
+   char   buff_snd[BUFF_SIZE+1];
+
+
+
+   sock  = socket( PF_INET, SOCK_DGRAM, 0);
+   
+   if( -1 == sock)
+   {
+      printf( "socket 생성 실패n");
+      exit( 1);
+   }
+
+   memset( &server_addr, 0, sizeof( server_addr));
+   server_addr.sin_family     = AF_INET;
+   server_addr.sin_port       = htons(PORT_NUMBER);
+   server_addr.sin_addr.s_addr= htonl(INADDR_ANY);
+
+   if( -1 == bind( sock, (struct sockaddr*)&server_addr, sizeof( server_addr) ) )
+   {
+      printf( "bind() 실행 에러n");
+      exit( 1);
+   }
+
+   while( 1)
+   {
+      client_addr_size  = sizeof( client_addr);
+      recvfrom( sock, buff_rcv, BUFF_SIZE, 0 , 
+                     ( struct sockaddr*)&client_addr, &client_addr_size);
+      printf( "receive: %s", buff_rcv);
+      
+      sprintf( buff_snd, "%s", buff_rcv);
+      sendto( sock, buff_snd, strlen( buff_snd)+1, 0, ( struct sockaddr*)&client_addr, sizeof( client_addr)); 
+   }
+}
