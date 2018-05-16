793ad91689ca9593815498f4f0f8dda9a6323874
Sanggu <anroniogi@icloud.com>
2018-05-17 05:38:30 +0900

    Update s.c

--- udp_tcp/s.c
+++ udp_tcp/s.c
@@ -1,72 +1,44 @@
 #include "headerFiles.h"
 
-#define BUF_LEN 128
+#define BUFF_SIZE 256
+#define PORT_NUMBER 50000
 
-
-int main(int argc, char *argv[])
+int   main( void)
 {
+   int   sock;
+   int   client_addr_size;
 
-        struct sockaddr_in server_addr, client_addr;
+   struct sockaddr_in   server_addr;
+   struct sockaddr_in   client_addr;
 
-        int s;
-        int len, msg_size;
+   char   buff_rcv[BUFF_SIZE+1];
 
-        char buf[BUF_LEN+1];
+   sock  = socket( PF_INET, SOCK_DGRAM, 0);
+   
+   if( -1 == sock)
+   {
+      printf( "socket 생성 실패\n");
+      exit( 1);
+   }
 
-        if(argc != 2){
-                printf("usage: %s port\n", argv[0]);
-                exit(0);
-        }
+   memset( &server_addr, 0, sizeof( server_addr));
+   server_addr.sin_family     = AF_INET;
+   server_addr.sin_port       = htons(PORT_NUMBER);
+   server_addr.sin_addr.s_addr= htonl(INADDR_ANY);
 
-        /* 소켓 생성 */
-        if((s = socket(PF_INET, SOCK_DGRAM, 0))&lt;0){
-                printf("server : Can`t open stream socket. ");
-                exit(0);
-        }
+   if( -1 == bind( sock, (struct sockaddr*)&server_addr, sizeof( server_addr) ) )
+   {
+      printf( "bind() 실행 에러\n");
+      exit( 1);
+   }
+   client_addr_size  = sizeof( client_addr);
 
-
-        /* server_addr을 NULL으로 초기화 */
-        bzero((char *)&amp;server_addr, sizeof(server_addr));
-        bzero((char *)&amp;client_addr, sizeof(client_addr));
-
-        /*server_addr 세팅 */
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-        server_addr.sin_port = htons(atoi(argv[1]));
-
-        /* bind() 호출 */
-        if(bind(s, (struct sockaddr *)&amp;server_addr, sizeof(server_addr)) &lt; 0){
-                printf("Server : Can,t bind local address. \n");
-                exit(1);
-        }
-
-
-        len = sizeof(struct sockaddr);
-
-
-        /* iterative echo 서비스 수행 */
-
-        while(1){
-                printf("Server : wating request.\n");
-
-                /* 연결요청을 기다림 */
-
-                if(msg_size = recvfrom(s, buf, BUF_LEN, 0 , (struct sockaddr *)&amp;client_addr, &amp;len) == -1){
-                        printf("recvfrom error\n");
-                        exit(0);
-                }
-
-                printf("server : A client data received. \n");
-
-                if(sendto(s, buf, msg_size, 0,  (struct sockaddr *)&amp;client_addr, len) &lt; 0){
-                        exit(0);
-                }
-
-                printf("sendto complete\n");
-
-        }
-
-
-        return 0;
-
+   while( 1)
+   {
+      printf("\n");
+      recvfrom( sock, buff_rcv, BUFF_SIZE, 0 , (struct sockaddr*)&client_addr, &client_addr_size);
+      printf( "receive: %s", buff_rcv);
+      
+      sendto(sock, buff_rcv, strlen(buff_rcv)+1, 0, (struct sockaddr*)&client_addr, sizeof( client_addr)); 
+   }
 }
