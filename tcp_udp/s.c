6f94563dd7c3d54ea15d59a7461d75cd9f22f82a
anroniogi <anroniogi@icloud.com>
2018-05-17 05:14:07 +0900

    다시

--- tcp_udp/s.c
+++ tcp_udp/s.c
@@ -1,6 +1,32 @@
 #include "headerfiles.h"
 
+#define MAXBUF 1024
+#define 
+
+
+
 int main(int argc, char *argv[]){
+    
+    int server_sockfd, client_sockfd;
+    int state, client_len;
+    int pid;
+    struct sockaddr_in clientaddr, serveraddr;
+    char buf[MAXBUF];
+    char tcp[] = "TCP : "
+    char buff_snd[MAXBUF];
+
+
+    //TCP SOCKET 생성
+    server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(server_sockfd < 0){
+        perror("socket error : ");
+        exit(0);
+    }
+
+    //서버에서 받을 클라이언트 주소, 포트주소 설정
+
+
+
 
     return 0;
 }
